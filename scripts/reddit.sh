2a933fcc91ccfc428840bc27eeb4230f063afc9c
Peter Kosyh <p.kosyh@gmail.com>
2021-02-18 10:11:57 +0300

    reddit link

--- scripts/reddit.sh
+++ scripts/reddit.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if [ "$#" -eq 0 ]; then
-	echo "No reddit"
+	echo "No reddit/link"
 	exit 1
 fi
-curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" https://www.reddit.com/r/"$1"/.rss | atom2rss | rss2gmi -h -r
+if echo "$1" | grep -q "^https:"; then
+	curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" "$1/.rss" | atom2rss | rss2gmi -h -r
+else
+	curl -s -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" https://www.reddit.com/r/"$1"/.rss | atom2rss | rss2gmi -h -r
+fi
